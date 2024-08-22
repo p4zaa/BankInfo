bec7f71e5f3051025f8ed070060d040264ef1eeb
Pa <56890167+p4zaa@users.noreply.github.com>
2024-08-22 14:14:11 +0700

    Optimize function

--- BankInfo/utils.py
+++ BankInfo/utils.py
@@ -1,8 +1,9 @@
 from BankInfo import banks_info
 
 def get_bank_info(country_code, bank_code, value=None):
-    if value:
-        data = banks_info.get(country_code).get(bank_code).get(value)
-    else:
-        data = banks_info.get(country_code).get(bank_code)
-    return data
+    # Safely retrieve the bank information
+    country_data = banks_info.get(country_code, {})
+    bank_data = country_data.get(bank_code, {})
+    
+    # If a specific value is provided, get that value; otherwise, return the whole bank data
+    return bank_data.get(value) if value else bank_data
